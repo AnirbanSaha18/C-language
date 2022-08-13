8d2e1f6bf65f15200373741ed7556cb823f5177e
Anirban Saha <108086878+AnirbanSaha18@users.noreply.github.com>
2022-08-13 10:42:13 +0530

    Create Test1.c

---
+++ Test1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int x,y;
+printf("Enter the number");
+scanf("%d %d",&x,&y);
+x=x+y;
+printf("Sum of two number is %d",x);
+getch();
+}
+
+
+
+
+ 
+  
+  
